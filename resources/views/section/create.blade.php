ec81f73e3b3f8d2947b31b0e9bec8502f0460eff
宝贝刀刀 <625117069@qq.com>
2019-01-05 20:30:26 +0800

    fix bug

--- resources/views/section/create.blade.php
+++ resources/views/section/create.blade.php
@@ -14,25 +14,30 @@
     <div class="card">
         <div class="card-header">
             <h3 class="card-title">Section</h3>
-            <a href="{{url('/admin/exam/' . $exam['id'])}}" class="btn btn-sm btn-outline-danger btn-icon ml-auto"><i class="fe fe-x"></i></a>
+            <a href="{{url('/admin/exam/' . $exam['id'])}}" class="btn btn-sm btn-outline-danger btn-icon ml-auto"><i
+                        class="fe fe-x"></i></a>
         </div>
         <div class="card-body">
             {!! Form::open(['method' => 'POST', 'route'=> 'section.store']) !!}
             <div class="form-group" hidden>
                 <label class="form-label">Exam ID</label>
-                <input name="exam_id" class="form-control" placeholder="Exam ID" value="{{ $exam['id'] or '' }}" readonly/>
+                <input name="exam_id" class="form-control" placeholder="Exam ID" value="{{ $exam['id'] or '' }}"
+                       readonly/>
             </div>
             <div class="form-group">
                 <label class="form-label">Number (考试日)</label>
-                <input name="number" class="form-control" placeholder="Number" value="{{ count($exam['section_list']) + 1 }}" readonly/>
+                <input name="number" class="form-control" placeholder="Number"
+                       value="{{ count($exam['section_list']) + 1 }}" readonly/>
             </div>
             <div class="form-group">
                 <label class="form-label">Exam Time (时间格式：HH:mm ，示例：09:30)</label>
-                {{--<input name="exam_time" class="form-control" placeholder="Exam Time" value="{{ $exam['exam_time'] or '' }}" readonly/>--}}
-                @include('public.datetime_picker', ['id' => 'examTime', 'name' => 'exam_time', 'placeholder' => 'exam_time', 'value' => old('exam_time'), 'format' => 'hh:ii', 'startView' => 0])
+                <input name="exam_time" class="form-control" placeholder="Exam Time" type="text"
+                       value="{{old('exam_time')}}"/>
+                {{--@include('public.datetime_picker', ['id' => 'examTime', 'name' => 'exam_time', 'placeholder' => 'exam_time', 'value' => old('exam_time'), 'format' => 'hh:ii', 'startView' => 0])--}}
             </div>
             <div class="form-footer text-center">
-                <button type="submit" class="btn btn-sm btn-outline-warning btn-icon"><i class="fe fe-send"></i>Save 保存</button>
+                <button type="submit" class="btn btn-sm btn-outline-warning btn-icon"><i class="fe fe-send"></i>Save 保存
+                </button>
             </div>
             {!! Form::close() !!}
         </div>
